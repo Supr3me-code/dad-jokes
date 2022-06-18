9cc30c0c02132cc1817e675f34d268ec24a1612f
Dishant Arora <dishantarora16@gmail.com>
2022-06-19 01:11:11 +0530

    added current tab listener

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
+import { getActiveTabURL } from "./utils.js";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
 
-const onPlay = e => {};
+const onPlay = (e) => {};
 
-const onDelete = e => {};
+const onDelete = (e) => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => {};
 
 document.addEventListener("DOMContentLoaded", () => {});
