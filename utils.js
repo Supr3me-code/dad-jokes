54d418276b02ecba01cf2225b5986534929ac9b8
Dishant Arora <dishantarora16@gmail.com>
2022-06-21 08:17:24 +0530

    updated base

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
 export async function getActiveTabURL() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+  
+    return tabs[0];
 }
+
